6a1c8490d55fbfdc8e7cc8e428a98cdeffef4c0b
ambika <abhi.fullstackdeveloper@gmail.com>
2019-06-25 08:52:59 +0530

    new

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
 	<Router>
