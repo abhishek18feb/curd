c75d43a6c0319f5b6248286ed7911e75e65a9e43
Abhishek Gupta <abhishek@braintechnosys.com>
2019-07-02 19:01:32 +0530

    new change

--- src/Backend/Employee/Add/Add.js
+++ src/Backend/Employee/Add/Add.js
@@ -1,61 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Layout from '../../../Layout/Layout';
 import './Add.css';
 
-class Add extends React.Component{
-	constructor(props) {
-	    super(props);
+class Add extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        	formControls:{
+        		firstname:{
+        			value: ''
+        		},
+        		lastname:{
+        			value:''
+        		},
+        		email:{
+        			value:''
+        		},
+        		mobile:{
+        			value:''
+        		}
+        	}
+        }
+	}
+
+
+    changeHandler = event => {
+	    const name = event.target.name;
+	    const value = event.target.value;
 	    
-	  }
-	render(){
-		return(
-			<Layout>
+	    this.setState({
+	        formControls: {
+	            ...this.state.formControls,
+	            [name]: {
+	            ...this.state.formControls[name],
+	            value
+	          }
+	        }
+	    });
+	}
+
+    render() {
+        return (
+            <Layout>
 				<div className="container">
-					   <form>
-					      <div className="row">
-					         <div className="col-25">
-					            <label for="fname">First Name</label>
-					         </div>
-					         <div className="col-75">
-					            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
-					         </div>
-					      </div>
-					      <div className="row">
-					         <div className="col-25">
-					            <label for="lname">Last Name</label>
-					         </div>
-					         <div className="col-75">
-					            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
-					         </div>
-					      </div>
-					      <div className="row">
-					         <div className="col-25">
-					            <label for="country">Country</label>
-					         </div>
-					         <div className="col-75">
-					            <select id="country" name="country">
-					               <option value="australia">Australia</option>
-					               <option value="canada">Canada</option>
-					               <option value="usa">USA</option>
-					            </select>
-					         </div>
-					      </div>
-					      <div className="row">
-					         <div className="col-25">
-					            <label for="subject">Subject</label>
-					         </div>
-					         <div className="col-75">
-					            <textarea id="subject" name="subject" placeholder="Write something.." style= {{height:'200px' }}></textarea>
-					         </div>
-					      </div>
+					   	<form onSubmit={this.handleSubmit}>
+					      	<div className="row">
+					         	<div className="col-25">
+					            	<label htmlFor="fname">First Name</label>
+					         	</div>
+					         	<div className="col-75">
+					            	<input type="text" id="fname" name="firstname" 
+					            		value={this.state.formControls.firstname.value} 
+					            		placeholder="Your name.." onChange={this.changeHandler}  />
+					         	</div>
+					      	</div>
+					      	<div className="row">
+					         	<div className="col-25">
+					           		<label htmlFor="lname">Last Name</label>
+					         	</div>
+					         	<div className="col-75">
+					            	<input type="text" id="lname" name="lastname" 
+					            	value={this.state.formControls.lastname.value} 
+					            	placeholder="Your last name.." onChange={this.changeHandler}  />
+					         	</div>
+					      	</div>
+					   		<div className="row">
+					         	<div className="col-25">
+					           		<label htmlFor="email">Email</label>
+					         	</div>
+					         	<div className="col-75">
+					            	<input type="text" id="email" name="email" 
+					            	value={this.state.formControls.email.value}
+					            	placeholder="Your email.." onChange={this.changeHandler}  />
+					         	</div>
+					      	</div>
+					      	<div className="row">
+					         	<div className="col-25">
+					           		<label htmlFor="mobile">Mobile</label>
+					         	</div>
+					         	<div className="col-75">
+					            	<input type="text" id="mobile" name="mobile"
+					            	value={this.state.formControls.mobile.value}
+					            	 placeholder="Your mobile.." onChange={this.changeHandler}  />
+					         	</div>
+					      	</div>
+					     
 					      <div className="row">
 					         <input type="submit" value="Submit" />
 					      </div>
-					   </form>
+					   	</form>
 				</div>
 			</Layout>
-		);
-	}
+        );
+    }
 }
 
 export default Add;
--- src/Partials/header/header.js
+++ src/Partials/header/header.js
@@ -9,14 +9,14 @@ import {
 } from "react-router-dom";
 
 const header = ()=>(
-		<div class="navbar">
+		<div className="navbar">
 		  <NavLink to="/" exact>Home</NavLink>
 		  <a href="#news">News</a>
-		  <div class="dropdown">
-		    <button class="dropbtn">Employee 
-		      <i class="fa fa-caret-down"></i>
+		  <div className="dropdown">
+		    <button className="dropbtn">Employee 
+		      <i className="fa fa-caret-down"></i>
 		    </button>
-		    <div class="dropdown-content">
+		    <div className="dropdown-content">
 		      <NavLink to="/employee/add">Employee Add</NavLink>
 		      <a href="#">Link 2</a>
 		      <a href="#">Link 3</a>
